739a2d549f11d98bd3ccd811b7f58191b9e27e31
lMwenda <Lukemwen@hotmail.com>
2022-08-28 14:15:48 +0100

    :hammer: Added Production URL

--- utils/exportedDefinitions.ts
+++ utils/exportedDefinitions.ts
@@ -1,6 +1,6 @@
 import { Prisma } from "@prisma/client";
 
-export const BASE_URL = "http://localhost:3000/"; // "https://new-healthcare.vercel.app/"; // ONCE PUSHING TO PRODUCTION CHANGE BASEURL TO THE PRODUCTION URL;
+export const BASE_URL = "https://new-healthcare.vercel.app/"; // "https://new-healthcare.vercel.app/"; // ONCE PUSHING TO PRODUCTION CHANGE BASEURL TO THE PRODUCTION URL;
 
 export type TSession = Prisma.SessionGetPayload<{ 
   include: { sessionAuthor: true }
